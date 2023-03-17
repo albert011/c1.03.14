8889a61be1dd3e423c0d19c48cfb3e22d7a6d1a3
vicgrabru <vgravanbru@gmail.com>
2023-03-17 18:06:56 +0100

    Eliminado import sin utilizar

--- src/main/java/acme/roles/Lecturer.java
+++ src/main/java/acme/roles/Lecturer.java
@@ -2,7 +2,6 @@
 package acme.roles;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
