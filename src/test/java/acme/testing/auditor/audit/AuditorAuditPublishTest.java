e5dd1eb8eec508beea4fd960baaa7a66c4da78cc
vicgrabru <vgravanbru@gmail.com>
2023-09-25 16:45:15 +0200

    Finished all testing for auditorAudit

--- src/test/java/acme/testing/auditor/audit/AuditorAuditPublishTest.java
+++ src/test/java/acme/testing/auditor/audit/AuditorAuditPublishTest.java
@@ -1,10 +1,14 @@
 
 package acme.testing.auditor.audit;
 
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.entities.audit.Audit;
 import acme.testing.TestHarness;
 
 public class AuditorAuditPublishTest extends TestHarness {
@@ -17,19 +21,77 @@ public class AuditorAuditPublishTest extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/auditor/audit/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test100Positive() {
-		//TODO
+	public void test100Positive(final int index, final String code) {
+		super.signIn("auditor1", "auditor1");
+
+		super.clickOnMenu("Auditor", "List my audits");
+		super.checkListingExists();
+		super.sortListing(2, "asc");
+
+		super.clickOnListingRecord(index);
+		super.checkFormExists();
+		super.checkSubmitExists("Publish audit");
+		super.clickOnSubmit("Publish audit");
+		super.checkNotErrorsExist();
+
+		super.signOut();
 	}
 
-	@ParameterizedTest
-	@CsvFileSource(resources = "/auditor/audit/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Test
 	public void test200Negative() {
-		//TODO
+
 	}
 
-	@ParameterizedTest
-	@CsvFileSource(resources = "/auditor/audit/publish-hacking.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Test
 	public void test300Hacking() {
-		//TODO
+		Collection<Audit> audits;
+		String param;
+
+		audits = this.repository.findManyAuditsByAuditorUsername("auditor1");
+
+		for (final Audit audit : audits) {
+			param = String.format("id=%d", audit.getId());
+
+			super.checkLinkExists("Sign in");
+			super.request("/auditor/audit/publish", param);
+			super.checkPanicExists();
+
+			super.signIn("administrator", "administrator");
+			super.request("/auditor/audit/publish", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("company1", "company1");
+			super.request("/auditor/audit/publish", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("student1", "student1");
+			super.request("/auditor/audit/publish", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("auditor2", "auditor2");
+			super.request("/auditor/audit/publish", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("lecturer1", "lecturer1");
+			super.request("/auditor/audit/publish", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("assistant1", "assistant1");
+			super.request("/auditor/audit/publish", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			if (audit.isPublished()) {
+				super.signIn("auditor1", "auditor1");
+				super.request("/auditor/audit/publish", param);
+				super.checkPanicExists();
+				super.signOut();
+			}
+		}
 	}
 }
