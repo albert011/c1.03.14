2edc628df6c1f9856841336a7c657059c3ee4d7e
vicgrabru <vgravanbru@gmail.com>
2023-09-27 17:47:25 +0200

    Added tests for AuditorAuditRecordShow

--- src/test/java/acme/testing/auditor/auditRecord/AuditorAuditRecordTestRepository.java
+++ src/test/java/acme/testing/auditor/auditRecord/AuditorAuditRecordTestRepository.java
@@ -6,11 +6,15 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.Query;
 
 import acme.entities.audit.Audit;
+import acme.entities.audit.AuditRecord;
 import acme.framework.repositories.AbstractRepository;
 
 public interface AuditorAuditRecordTestRepository extends AbstractRepository {
 
 	@Query("select ar.audit from AuditRecord ar where ar.audit.auditor.userAccount.username = :username")
 	Collection<Audit> findManyAuditsWithAuditRecordByAuditorUsername(String username);
+
+	@Query("select ar from AuditRecord ar where ar.audit.auditor.userAccount.username = :username")
+	Collection<AuditRecord> findManyAuditRecordsByAuditorUsername(String username);
 
 }
